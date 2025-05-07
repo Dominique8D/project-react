bbdb5ff1dc3171350e030d44b2430e2f955933f9
Dominique8D <dejong.dominique@gmail.com>
2025-05-07 13:25:48 +0000

    Add deploy cmd and phaser example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import './i18n';
 import useLangTranslation from './custom-hooks/useLangTranslation';
+import { Link } from '@mui/material';
 
 function App() {
   const { t } = useLangTranslation("common");
@@ -9,6 +10,9 @@ function App() {
       <header className="App-header">
         <img src="site-logo.png" className="App-logo" alt="logo" />
         <p> {t('title')} </p>
+          <Link href="https://dominique8d.github.io/project-phaser/">
+            https://dominique8d.github.io/project-phaser/
+          </Link>
       </header>
     </div>
   );
